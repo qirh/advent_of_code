57edf3355de8a48d73ce9bc8c91d0376a5e6dd9d
saleh <saleh@alghusson.com>
2024-12-15 19:27:35 -0500

    add types

--- 2024/day1/day1_historian_hysteria.py
+++ 2024/day1/day1_historian_hysteria.py
@@ -1,43 +1,51 @@
 # https://adventofcode.com/2024/day/1
 
+import os
 from collections import Counter
 
-def get_list_pairs():
+
+def get_list_pairs() -> list[tuple[int, int]]:
     pairs = []
-    with open('input') as input_file:
-        for line in input_file:   
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    input_path = os.path.join(script_dir, 'input')
+
+    with open(input_path) as input_file:
+        for line in input_file:
             pair = line.split()
             pairs.append(tuple(map(int, pair)))
     return pairs
 
-def separate_pairs(pairs):
+
+def separate_pairs(pairs: list[tuple[int, int]]) -> tuple[list[int], list[int]]:
     return list(zip(*pairs))
 
-def calculate_total_distance(l1, l2):
+
+def calculate_total_distance(l1: list[int], l2: list[int]) -> int:
     distance = 0
     for n1, n2 in zip(sorted(l1), sorted(l2)):
-        distance += abs(n1-n2)
+        distance += abs(n1 - n2)
     return distance
 
+
+# part1
 pairs = get_list_pairs()
 l1, l2 = separate_pairs(pairs)
 distance = calculate_total_distance(l1, l2)
-
-# part1
-print(f'distance: {distance}') # 3574690
+print(f"distance: {distance}")  # 3574690
 
 
-def get_counter(l):
+def get_counter(l: list[int]) -> Counter[int]:
     return Counter(l)
-    
-def calculate_similarity(l, c):
+
+
+def calculate_similarity(l: list[int], c: Counter[int]) -> int:
     similarity = 0
     for n in l:
-        similarity += (n * c.get(n, 0))
+        similarity += n * c.get(n, 0)
     return similarity
 
-c = get_counter(l2)
-similarity = calculate_similarity(l1, c)
 
 # part2
-print(f'similarity: {similarity}')
+c = get_counter(l2)
+similarity = calculate_similarity(l1, c)
+print(f"similarity: {similarity}")  # 22565391
